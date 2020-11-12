640d0e6e5230a723859abe53f3170183e50ee708
Wes <wesleySalesberry@hotmail.com>
2020-11-11 21:27:30 -0500

    moved items into their own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,22 @@
-import React, {useState, useEffect} from 'react'
-import './App.css';
+import React from 'react'
+
 import { Navbar } from './component/Navbar/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+
+import { CardDisplay } from './component/Display/CardDisplay';
 import { Card } from './component/Card/Card';
 
-import api from './Api/api'
-
 function App() {
-  const [response, setResponse] = useState()
 
-  useEffect(() => {
-    getTest()
-  }, [])
-    const getTest = async () =>{
-      try{
-        const URL = '/hour'
-        const res = await api.get(URL)
 
-        console.log(res.data[0].properties)
-        for(let x = 0; x > res.length; x++){
-          console.log(res.data[x].properties)
-        }
-        
-      }catch(err){
-        console.log(`${err}`)
-      }
-    } 
-
-    //console.log(response)
   return (
     
     <BrowserRouter>
       <Navbar/>
-      <Card/>
+      <CardDisplay name={"hour"} />
+     
     </BrowserRouter>
   );
 }
