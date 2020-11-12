38228ef66450e4e19a924b4ae48000c424b8f26c
Wes <wesleySalesberry@hotmail.com>
2020-11-12 14:22:48 -0500

    add the db and route for register

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const cors = require('cors')
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}))
 app.use(cors())
+
+const connectDB = require('./database/mainDB')
+
+connectDB()
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -20,10 +25,12 @@ NEDB used for testing
 
 const geoLocation = require('./routes/geolocation');
 const currentDay = require('./routes/history')
+const register = require('./routes/auth/register')
 app.get('/', (req, res) => res.send('API Running'));
 
 
 app.use(geoLocation);
 app.use(currentDay)
+app.use(register)
 
 app.listen(PORT, () => console.log(`> Server Started On: http://localhost:${PORT}`));
