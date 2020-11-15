63ff39d968e0b5f728793358c3e4316ff8d91275
Wes <wesleySalesberry@hotmail.com>
2020-11-14 20:10:56 -0500

    added search and a function to find the coords from the city

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
+const { response } = require("express");
 
 
 
 module.exports = {
-     async myData(name) {
+     async hourlyData(name) {
         try{
             const URL = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_${name}.geojson`
             let res = await axios.get(URL)
@@ -12,5 +13,73 @@ module.exports = {
             console.log(`${err}`)
         }
     },
+
+    async searchFunction(latitude, longitude){
+        const URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=50`
+        try {
+            let results = await axios.get(URL)
+            return results.data.features
+            
+        } catch (err) {
+            console.log(`Error on 22${err}`)
+        }
+    },
+
+    
+    async geocode(location, callback) {
+        try{
+            const GEO_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${process.env.MAPBOX_API}`;
+            let response = await axios.get(GEO_URL)
+            return response.data.features[0].center
+
+        }catch(error){
+            console.log(`ERROR on 42: ${error}`)
+        }
+	
+    },
+
+    isEmpty(obj){
+        for(let key in obj){
+            if(obj.hasOwnProperty(key)){
+                return false
+            }
+        }
+        return true
+    }
+
 }
+
+
+// module.exports = {
+//     async searchFunction(latitude, longitude){
+//         const URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=50`
+//         try {
+//             let results = await axios.get(URL)
+//             return results.data.features
+//         } catch (err) {
+//             console.log(`${err}`)
+//         }
+//     },
+
+//         async geocode(location, callback) {
+//         try{
+//             const GEO_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${process.env.MAPBOX_API}`;
+//             let results = await axios.get(GEO_URL)
+//             const latLong = results.data.features[0].center
+//             callback({
+//                         latitude: latLong[1],
+//                         longitude: latLong[0]
+//                     })
+
+//         }catch(error){
+//             console.log(` ${error}`)
+//         }
+	
+//     },
+
+// }
+
+
+
+		
 
