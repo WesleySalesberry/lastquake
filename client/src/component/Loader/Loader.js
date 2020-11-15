65e83ee4044ca90f6d87b0744e7b0caaa269a625
Wes <wesleySalesberry@hotmail.com>
2020-11-15 08:33:58 -0500

    loader

---
+++ client/src/component/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export const Loader = () => {
+    return (
+        <Fragment>
+            <div class="d-flex justify-content-center">
+               <div class="spinner-border" role="status">
+                   <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
