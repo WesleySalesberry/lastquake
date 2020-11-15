89a73769e7066b408df9a336bebcf112aec94aaf
Wes <wesleySalesberry@hotmail.com>
2020-11-14 20:14:18 -0500

    need to restructure this

--- routes/geolocation.js
+++ routes/geolocation.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const addData = require('../utils/utils')
+// const express = require('express')
+// const router = express.Router()
+// const addData = require('../utils/utils')
 
 
-//TODO: redo to store in an array
-router.post('/api/location', (req, res) => {
-    const data = req.body
-    //data.timestamp = timestamp
-    //addData(data)
-    console.log(data)
-    res.json(data)
-})
+// //TODO: redo to store in an array
+// router.post('/api/location', (req, res) => {
+//     const data = req.body
+//     //data.timestamp = timestamp
+//     //addData(data)
+//     console.log(data)
+//     res.json(data)
+// })
 
 
-module.exports = router
+// module.exports = router
