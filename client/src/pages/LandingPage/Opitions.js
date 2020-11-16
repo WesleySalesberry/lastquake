033215f6c624f477136c97ffb567a0450a7ca88b
Wes <wesleySalesberry@hotmail.com>
2020-11-15 19:43:50 -0500

    setting up items for the drop down

---
+++ client/src/pages/LandingPage/Opitions.js
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const options = [
+        { 
+            id: uuidv4(),
+            name: 'Oakland'
+        },
+        { 
+            id: uuidv4(),
+            name: 'Portland'
+        },
+        { 
+            id: uuidv4(),
+            name: 'Pink Hill'
+        },
+       
+    ]
