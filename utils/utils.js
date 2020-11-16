a1ce5402e0d5f71b0a7cf209caa364ba404ff43e
Wes <wesleySalesberry@hotmail.com>
2020-11-16 09:13:47 -0500

    cleaned up and restructured my code

--- utils/utils.js
+++ utils/utils.js
@@ -2,9 +2,8 @@ const axios = require("axios");
 const { response } = require("express");
 
 
-
 module.exports = {
-     async hourlyData(name) {
+    async hourlyData(name) {
         try{
             const URL = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_${name}.geojson`
             let res = await axios.get(URL)
@@ -13,31 +12,31 @@ module.exports = {
             console.log(`${err}`)
         }
     },
-
     async searchFunction(latitude, longitude){
         const URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=50`
         try {
             let results = await axios.get(URL)
             return results.data.features
-            
         } catch (err) {
-            console.log(`Error on 22${err}`)
+            console.log(`${err}`)
         }
     },
 
-    
-    async geocode(location, callback) {
+        async geocode(location, callback) {
         try{
             const GEO_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${process.env.MAPBOX_API}`;
-            let response = await axios.get(GEO_URL)
-            return response.data.features[0].center
+            let results = await axios.get(GEO_URL)
+            const latLong = results.data.features[0].center
+            return callback({
+                        latitude: latLong[1],
+                        longitude: latLong[0]
+                    })
 
         }catch(error){
-            console.log(`ERROR on 42: ${error}`)
+            console.log(` ${error}`)
         }
 	
     },
-
     isEmpty(obj){
         for(let key in obj){
             if(obj.hasOwnProperty(key)){
@@ -50,34 +49,7 @@ module.exports = {
 }
 
 
-// module.exports = {
-//     async searchFunction(latitude, longitude){
-//         const URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=50`
-//         try {
-//             let results = await axios.get(URL)
-//             return results.data.features
-//         } catch (err) {
-//             console.log(`${err}`)
-//         }
-//     },
 
-//         async geocode(location, callback) {
-//         try{
-//             const GEO_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${process.env.MAPBOX_API}`;
-//             let results = await axios.get(GEO_URL)
-//             const latLong = results.data.features[0].center
-//             callback({
-//                         latitude: latLong[1],
-//                         longitude: latLong[0]
-//                     })
-
-//         }catch(error){
-//             console.log(` ${error}`)
-//         }
-	
-//     },
-
-// }
 
 
 
