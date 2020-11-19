2e630c3479298baeedf6e97529a4600988fae5d9
Wes <wesleySalesberry@hotmail.com>
2020-11-19 18:42:52 -0500

    added another array

--- client/src/pages/LandingPage/Opitions.js
+++ client/src/pages/LandingPage/Opitions.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const options = [
+export const option_1 = [
         { 
             id: uuidv4(),
             name: 'Oakland'
@@ -14,4 +14,34 @@ export const options = [
             name: 'Pink Hill'
         },
        
+    ]
+
+    export const option_2 = [
+        { 
+            id: uuidv4(),
+            className: "dropdown-item", 
+            name: 'Past Hour',
+            value: "hour"
+        },
+        { 
+            id: uuidv4(),
+            className: "dropdown-item", 
+            to: '#',
+            name: 'Past Day',
+            value: "day"
+        },
+        { 
+            id: uuidv4(),
+            className: "dropdown-item", 
+            to: '#',
+            name: 'Past Week',
+            value: "week"
+        },
+        { 
+            id: uuidv4(),
+            className: "dropdown-item", 
+            to: '#',
+            name: 'Past Month',
+            value: "month"
+        }
     ]
