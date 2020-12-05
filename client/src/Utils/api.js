05575d395ea252cb3f5c7fadd077f320022da62a
Wes <wesleySalesberry@hotmail.com>
2020-12-05 00:14:55 -0500

    inital token check

--- client/src/Utils/api.js
+++ client/src/Utils/api.js
@@ -1,23 +1,31 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: 'http://localhost:5000/api/',
 	
 });
 
+export const setAuthToken = token => {
+	if (token) {
+		axios.defaults.headers.common['x-auth-token'] = token;
+	} else {
+		delete axios.defaults.headers.common['x-auth-token'];
+	}
+};
+
 export const currentData = async (name) => {
 	try {
-		const response =  await api.get(`/${name}`)
+		const response =  await api.get(`${name}`)
 		return response
 	} catch (error) {
-		console.log(`${error}`)
+		console.log(`> Error from current data search: ${error}`)
 		
 	}
 }
 
 export const citySearch = async (city) => {
 	try {
-		const cityData = await api.get(`/search/${city}`)
+		const cityData = await api.get(`search/${city}`)
 		return cityData
 		
 	} catch (error) {
