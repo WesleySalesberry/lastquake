787c6ad8bcec4c4214656926e11404bc4726b5d4
Wes <wesleySalesberry@hotmail.com>
2020-12-11 17:05:37 -0500

    added the logout

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react'
 import {Link} from 'react-router-dom'
 import {Nav} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { logout } from '../../Redux/user/user.actions'
 
 
 
 // import DropdownButton from 'react-bootstrap/DropdownButton';
 // import Dropdown from 'react-bootstrap/Dropdown'
 
-export const Navbar = () => {
+const Navbar = ({ logout, isAuthenticated, loading}) => {
     return (
     <Nav className="bg-dark">
            <Nav.Item>
@@ -16,9 +19,23 @@ export const Navbar = () => {
         <Nav.Item>
             <Nav.Link href="/forms">Get an Account</Nav.Link>
         </Nav.Item>
+        <Nav.Item>
+            <Nav.Link onClick={logout}>Logout</Nav.Link>
+        </Nav.Item>
     </Nav>
     )
 }
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, {logout})(Navbar)
+
 
 
 
