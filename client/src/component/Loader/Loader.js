c4a6861f51dd956ccc4c711a5ae552774d4f5cde
Wes <wesleySalesberry@hotmail.com>
2020-12-10 21:36:44 -0500

    fixed the classname

--- client/src/component/Loader/Loader.js
+++ client/src/component/Loader/Loader.js
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react'
 export const Loader = () => {
     return (
         <Fragment>
-            <div class="d-flex justify-content-center">
-               <div class="spinner-border" role="status">
-                   <span class="sr-only">Loading...</span>
+            <div className="d-flex justify-content-center">
+               <div className="spinner-border" role="status">
+                   <span className="sr-only">Loading...</span>
                 </div>
             </div>
         </Fragment>
