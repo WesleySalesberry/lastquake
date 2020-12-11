0fdd5bef1d1007f0a664c2adddd4783ecb6f694f
Wes <wesleySalesberry@hotmail.com>
2020-12-10 21:43:12 -0500

    added alerts

--- client/src/Redux/root-reducer.js
+++ client/src/Redux/root-reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
+import { alertReducer } from './alert/alert';
 import { userReducer } from './user/user'
 
 export default combineReducers({
+    alert: alertReducer,
     user: userReducer
 })
