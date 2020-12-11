843e4521e911fc6d693cd766acc7f39141e310b4
Wes <wesleySalesberry@hotmail.com>
2020-12-11 17:01:52 -0500

    calling load user

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React,{Fragment, useEffect, } from 'react'
 
-import { Navbar } from './component/Navbar/Navbar';
+import Navbar from './component/Navbar/Navbar';
 import { Route } from 'react-router-dom';
 
 import { DisplayForms } from './Pages/Auth/DisplayForms';
 import { Landing } from './Pages/LandingPage/Landing';
 
+import { setAuthToken } from './Utils/api'
+import { loaduser } from './Redux/user/user.actions';
+import {store} from './Redux/store'
+import { Alert } from 'react-bootstrap';
+
+ if(localStorage.token){
+        setAuthToken(localStorage.token)
+    }
 
 export const App = () => {
- 
- 
+  
+  useEffect(() => {
+    store.dispatch(loaduser())
+  }, [])
+
   return (
       <Fragment>
        <Navbar />
+       <Alert />
         <Route exact path="/forms" component={DisplayForms}/>
         <Route exact path="/" component={Landing} />
       </Fragment>
