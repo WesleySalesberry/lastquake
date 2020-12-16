6598d2d754374872e9d40d2997a4931e0470bca5
Wes <wesleySalesberry@hotmail.com>
2020-12-15 21:12:57 -0500

    added a switch for the private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React,{Fragment, useEffect, } from 'react'
 
-import Navbar from './component/Navbar/Navbar';
-import { Route } from 'react-router-dom';
+import NavbarComponent from './component/Navbar/Navbar';
+import { Route, Switch } from 'react-router-dom';
 
 import { DisplayForms } from './Pages/Auth/DisplayForms';
 import { Landing } from './Pages/LandingPage/Landing';
@@ -9,9 +9,12 @@ import { Landing } from './Pages/LandingPage/Landing';
 import { setAuthToken } from './Utils/api'
 import { loaduser } from './Redux/user/user.actions';
 import {store} from './Redux/store'
-import { Alert } from 'react-bootstrap';
+
 import { FooterComponent } from './component/footer/Footer';
+import Alerts from './component/Alerts/Alert';
 import { Dashboard } from './Pages/Dashboard/Dashboard';
+
+import PrivateRoute from './component/Routeing/PrivateRoute';
 
 
  if(localStorage.token){
@@ -26,11 +29,13 @@ export const App = () => {
 
   return (
       <Fragment>
-       <Navbar />
-       <Alert />
-        <Route exact path="/forms" component={DisplayForms}/>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/dashboard" component={Dashboard}/>
+       <NavbarComponent />
+       <Alerts /> 
+       <Switch>
+          <Route exact path="/forms" component={DisplayForms}/>
+          <Route exact path="/" component={Landing} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+        </Switch>
       </Fragment>
   );
 }
